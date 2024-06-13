2f3dd67ae9a221b3c259bb201c7ca74d8d39cc6c
Alana Fernandes <48449273+alanaafsc@users.noreply.github.com>
2024-06-13 19:17:39 -0300

    Update Consumer.py

--- Model/Consumer.py
+++ Model/Consumer.py
@@ -4,25 +4,26 @@ import numpy as np
 class Consumer(ap.Agent):
 
     def setup(self, positions):
-        self.__position = positions[self.id-1, :]
+        self.__position = positions[self.id - 1, :]
         self.__leaved = False
+        self.information_level = np.random.uniform(0.5, 1.0)  # nível de informação do consumidor
 
     def leaved(self):
         return self.__leaved
 
     def agent_method(self, utility_values):
         if np.any(utility_values > 0):
-            # chose the product with max utility
+            # produto com maior utilidade
             pref_company_index = np.argmax(utility_values)
             self.__pref_company_index = pref_company_index
         else:
             self.leave_market()
 
     def leave_market(self):
-        #todo: if consumer has negative utility he leaves the market
-        pass
-    
+        self.__leaved = True
+
     def pref_company_id(self):
         return self.__pref_company_index
+    
     def position(self):
         return self.__position
